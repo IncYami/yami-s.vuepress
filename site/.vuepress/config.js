e09c57717a3a642117850581ccc7fab3c4589e39
Sekkon <56774713+IncYami@users.noreply.github.com>
2022-12-19 23:09:19 -0300

    Update config.js

--- site/.vuepress/config.js
+++ site/.vuepress/config.js
@@ -58,7 +58,7 @@ module.exports = {
     // permalink for posts
     ['blog-multidir', {
       postsDir: {
-        posts: 'posts/'
+        posts: 'posts/:slug'
       //posts: 'posts/:year/:month/:day/:slug'
       }
     }],
